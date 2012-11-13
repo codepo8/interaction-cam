72a5c9d246bf98a5a9b34b7dff8d767231b131fc
Christian Heilmann <chris.heilmann@gmail.com>
2012-11-13 13:30:44 +0000

    fixes+cleanup

--- copy.php
+++ copy.php
@@ -1,9 +1,12 @@
 <?php
-$file = rand();
+if ($_SERVER['SERVER_NAME'] !== 'localhost') {
+	die('this should only run locally!');
+}
+$file = date('Y-m-d-H-i-s');
 $contents = $_POST['contents'];
 $encodedData = str_replace(' ','+',$contents);
 $decodedData = base64_decode($encodedData);
-$fp = fopen('copies/'.$file.'.png', 'w');
+$fp = fopen('copies/'.$file.'.jpg', 'w');
 fwrite($fp, $decodedData);
 fclose($fp);
 ?>
--- interactioncam.js
+++ interactioncam.js
@@ -3,25 +3,41 @@
   var API_KEY = 'eb18642b5b220484864483b8e21386c3';
       //  ^ get your own at https://imgur.com/register/api_anon
       //    as it is limited to 50 uploads an hour!
-  var streaming    = false,
-      video        = document.querySelector('#video'),
+  var video        = document.querySelector('#video'),
       cover        = document.querySelector('#cover'),
       canvas       = document.querySelector('#canvas'),
-      ctx          = canvas.getContext('2d'),
       vidcontainer = document.querySelector('#videocontainer'),
       resetbutton  = document.querySelector('#resetbutton'),
       startbutton  = document.querySelector('#startbutton'),
       uploadbutton = document.querySelector('#uploadbutton'),
       urlfield     = document.querySelector('#uploaded input'),
-      urllink      = document.querySelector('#uploaded a'),
-      audio        = document.querySelectorAll('audio'),
-      width        = 600,
-      height       = 450,
-      state        = 'intro',
-      sounds       = {shutter: audio[0],rip: audio[1], takeoff: audio[2]};
-      img          = new Image();
-      img.src = 'mozfest.png';
-      setstate(state);
+      urllink      = document.querySelector('#uploaded a');
+      
+ var ctx    = canvas.getContext('2d'),
+     streaming    = false,
+     width  = 600,
+     height = 450,
+     state  = 'intro';
+
+ var audio = document.querySelectorAll('audio'),
+     sounds = {
+        shutter: audio[0],
+        rip:     audio[1],
+        takeoff: audio[2]
+      };
+
+  /* BRANDING */
+  var img = new Image(),
+      imgwidth = 600,
+      imgheight = 104;
+  img.src = 'mozfest.png';
+
+  if (location.hostname.indexOf('localhost')!== -1) {
+    document.querySelector('#imgurform').style.display = 'none';
+  }
+
+  setstate(state);
+
   function init() {
     navigator.getMedia = ( navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
@@ -56,7 +72,7 @@
     ctx.drawImage(video, 0, 0, width, finalheight);
     ctx.restore();
     ctx.scale(1, 1);
-    ctx.drawImage(img, 0, 450 - 104, 600, 104);
+    ctx.drawImage(img, 0, 450 - imgheight, imgwidth, imgheight);
   }
 
   function reshoot() {
@@ -73,28 +89,26 @@
 
   function initiateupload() {
     if (state === 'reviewing') {
+      sounds.takeoff.play();
       setstate('uploading');
       upload();
     }
   }
 
   function upload() {
-    var head = /^data:image\/(png|jpg);base64,/,
+    var head = /^data:image\/(png|jpeg);base64,/,
         data = '',
         fd = new FormData(),
         xhr = new XMLHttpRequest();
 
     setstate('uploading');
-    data = ('mozGetAsFile' in canvas) ?
-           canvas.mozGetAsFile('webcam.png') :
-           canvas.toDataURL('image/png').replace(head, '');
+    data = canvas.toDataURL('image/jpeg', 0.9).replace(head, '');
 
     if (location.hostname.indexOf('localhost')!== -1) {
-      document.querySelector('form').style.display = 'none';
       fd.append('contents', data);
       xhr.open('POST', 'copy.php');
       xhr.addEventListener('error', function(ev) {
-        console.log('Upload Error :');
+        console.log('Upload Error!');
       }, false);
       xhr.addEventListener('load', function(ev) {
         setstate('uploaded');
@@ -105,19 +119,17 @@
       fd.append('key', API_KEY);
       xhr.open('POST', 'http://api.imgur.com/2/upload.json');
       xhr.addEventListener('error', function(ev) {
-        console.log('Upload Error :');
+        console.log('Upload Error!');
       }, false);
       xhr.addEventListener('load', function(ev) {
         try {
           var links = JSON.parse(xhr.responseText).upload.links;
-          localStorage.interactionphotos = localStorage.interactionphotos + 
-                                            ' ' +
-                                           links.imgur_page.replace(/.*\/+/,'');
+          store(links.imgur_page.replace(/.*\/+/,''));
           urlfield.value = links.imgur_page;
           urllink.href = links.imgur_page;
           setstate('uploaded');
         } catch(e) {
-          console.log('Upload Error :' + e);
+          console.log('Upload Error!' + e);
         }
       }, false);
       xhr.send(fd);
@@ -127,6 +139,12 @@
  function setstate(newstate) {
     state = newstate;
     document.body.className = newstate;
+  }
+  function store(name) {
+    if (localStorage.interactionphotos === undefined) {
+      localStorage.interactionphotos = '';
+    }
+    localStorage.interactionphotos += ' '+ name;
   }
 
   /* Event Handlers */
@@ -138,16 +156,11 @@
       video.setAttribute('height', finalheight);
       canvas.width = width;
       canvas.height = finalheight;
-      ctx.drawImage(img, 0, 450-104, 600, 104);
+      ctx.drawImage(img, 0, 450 - 104, 600, 104);
       streaming = true;
       vidcontainer.classname = 'playing';
     }
   }, false);
-
-  video.addEventListener('timeupdate', function(ev) {
-      if (state !== 'reviewing') {
-      }
-  },false);
 
   document.addEventListener('keydown', function(ev) {
     if (ev.which === 32 || ev.which === 37 || ev.which === 39) {
@@ -167,7 +180,6 @@
     }
     if (ev.which === 39) {
       initiateupload();
-      sounds.takeoff.play();
     }
   },false);
 
